9edef1fa0138c90c7053b732ccf36064f23750b8
NervezXx <kelanjpa1@gmail.com>
2017-08-28 19:58:59 +0100

    Auto stash before merge of "master" and "origin/master"

--- src/main/java/dk/futte/blue/teamblep/blepcore/proxy/ServerProxy.java
+++ src/main/java/dk/futte/blue/teamblep/blepcore/proxy/ServerProxy.java
@@ -1,8 +1,17 @@
 package dk.futte.blue.teamblep.blepcore.proxy;
 
+import dk.futte.blue.teamblep.blepcore.BlepCore;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.ResourceLocation;
+
 /**
  * @author Blue
  */
 public class ServerProxy implements IProxy
 {
+    @Override
+    public void registerModel(ItemStack itemStack, ResourceLocation resourceLocation)
+    {
+        BlepCore.logger.error("Failed to register model for " + itemStack.getDisplayName() + ". This should not be done on server-side.");
+    }
 }
