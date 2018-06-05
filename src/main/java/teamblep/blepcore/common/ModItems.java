35486a93c244b6c9c1e35110da76578d53266363
bluemonster122 <boo122333@gmail.com>
2018-06-05 12:14:05 +0100

    Adds Model Register Event to ModItems

--- src/main/java/teamblep/blepcore/common/ModItems.java
+++ src/main/java/teamblep/blepcore/common/ModItems.java
@@ -1,10 +1,13 @@
 package teamblep.blepcore.common;
 
 import net.minecraft.item.Item;
+import net.minecraftforge.client.event.ModelRegistryEvent;
 import net.minecraftforge.event.RegistryEvent.Register;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.registry.GameRegistry.ObjectHolder;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 import teamblep.blepcore.BlepCore;
 import teamblep.blepcore.common.item.tools.ToolAxe;
 
@@ -19,5 +22,11 @@ public class ModItems {
   public static void onRegisterItems(Register<Item> event) {
     BlepCore.log("Registering Items");
     event.getRegistry().registerAll(new ToolAxe("example_axe"));
+  }
+
+  @SideOnly(Side.CLIENT)
+  @SubscribeEvent
+  public static void onRegisterModels(ModelRegistryEvent event) {
+    // NOTE: register Models here.
   }
 }
